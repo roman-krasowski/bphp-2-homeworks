1b683a1ec7ddcc7d91c6fb64176096ab82e85426
roman-krasowski <krasovskiyroman@gmail.com>
2020-11-24 23:55:52 +0300

    initial commit

---
+++ 003-operators/index.php
@@ -0,0 +1,30 @@
+<?php
+fwrite(STDOUT, "Введите Вашу фамилию:" . PHP_EOL);
+$surname = trim(fgets(STDIN));
+fwrite(STDOUT, "Введите Ваше имя:" . PHP_EOL);
+$firstName = trim(fgets(STDIN));
+fwrite(STDOUT, "Введите Ваше отчество:" . PHP_EOL);
+$middletName = trim(fgets(STDIN));
+
+/*
+$name = [$surname, $firstName, $middletName];
+for($i=0; $i<count($name); $i++) {
+  str_replace(substr($name[i], 0, 1), strtoupper(substr($name[i], 0, 1)), $name[i])
+};
+fwrite(STDOUT, $name);
+*/
+
+//преобразование первой буквы к верхнему регистру
+$surname = str_replace(substr($surname, 0, 1), strtoupper(substr($surname, 0, 1)), $surname);
+$firstName = str_replace(substr($firstName, 0, 1), strtoupper(substr($firstName, 0, 1)), $firstName);
+$middletName = str_replace(substr($middletName, 0, 1), strtoupper(substr($middletName, 0, 1)), $middletName);
+
+$fullName = $surname . " " . $firstName . " " . $middletName;
+$fullName = str_replace(substr($surname, 0, 1), strtoupper(substr($surname, 0, 1)), $surname);
+$surnameAndInitials = $surname . " " . substr($firstName, 0, 1) . "." . substr($middletName, 0, 1) . ".";
+$fio = substr($surname, 0, 1) . substr($firstName, 0, 1) . substr($middletName, 0, 1);
+
+fwrite(STDOUT, "Полное имя: " . $fullName . PHP_EOL);
+fwrite(STDOUT, "Фамилия и инициалы: " . $surnameAndInitials . PHP_EOL);
+fwrite(STDOUT, "Аббревиатура: " . $fio . PHP_EOL);
+
